7527486b32ba7bcb490e0f67fb72a61bd76a8bb2
developerwill <info@wilsongomes.me>
2022-12-22 13:12:09 +0100

    Filtering Projects with Enums

--- src/app.ts
+++ src/app.ts
@@ -131,7 +131,12 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         state.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            this.assignedProjects = projects.filter(project => {
+                if (this.type === 'active')
+                    return project.status === ProjectStatus.Active;
+                else
+                    return project.status === ProjectStatus.Finished;
+            });
             this.renderProjects();
         });
 
@@ -141,6 +146,8 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+
+        listEl.innerHTML = '';
 
         for (const projectItem of this.assignedProjects) {
             const listItem = document.createElement('li');
