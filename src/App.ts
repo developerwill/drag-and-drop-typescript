d8ffd8563e9cce2a3423bd7800bc0574b29781cd
developerwill <info@wilsongomes.me>
2022-12-22 16:45:29 +0100

    Adding a Droppable Area

--- src/App.ts
+++ src/App.ts
@@ -177,7 +177,8 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
     @autoBind
     dragStartHandler(event: DragEvent): void {
-        console.log(event);
+       event.dataTransfer!.setData('text/plain', this.project.id);
+       event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent): void {
@@ -209,9 +210,12 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
 
     @autoBind
-    dragOverHandler(_: DragEvent) {
-        const listEl = this.element.querySelector('ul')!;
-        listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent) {
+        if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
+            const listEl = this.element.querySelector('ul')!;
+            listEl.classList.add('droppable');
+        }
     }
 
     @autoBind
