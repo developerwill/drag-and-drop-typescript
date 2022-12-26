ddad29560954e8a8d8a471ac5683761f53096639
developerwill <info@wilsongomes.me>
2022-12-26 16:52:22 +0100

    Using ES Modules

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,92 +1,91 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../utils/validation.ts" />
-/// <reference path="../state/project-state.ts" />
+import { Component } from './base-component.js';
+import { Validatable, validate } from '../utils/validation.js';
+import { autoBind } from '../decorators/autobind.js';
+import { state } from '../state/project-state.js';
 
-namespace App {
-    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
-        // Form fields
-        titleInputElement: HTMLInputElement;
-        descriptionInputElement: HTMLInputElement;
-        peopleInputElement: HTMLInputElement;
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    // Form fields
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
-        constructor() {
-            super('project-input', 'app', true, 'user-input');
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
 
-            // Stores form fields into individual properties
-            this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-            this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-            this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        // Stores form fields into individual properties
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
-            this.configure();
+        this.configure();
+    }
+
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    renderContent() {
+    }
+
+    // This method returns a tuple of 2 strings and a number
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+        };
+
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]; // Returns a tuple. The + sign before the variable converts to int
         }
+    }
 
-        configure() {
-            this.element.addEventListener('submit', this.submitHandler);
-        }
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
 
-        renderContent() {}
-
-        // This method returns a tuple of 2 strings and a number
-        private gatherUserInput(): [string, string, number] | void {
-            const enteredTitle = this.titleInputElement.value;
-            const enteredDescription = this.descriptionInputElement.value;
-            const enteredPeople = this.peopleInputElement.value;
-
-            const titleValidatable: Validatable = {
-                value: enteredTitle,
-                required: true
-            };
-
-            const descriptionValidatable: Validatable = {
-                value: enteredDescription,
-                required: true,
-                minLength: 5
-            };
-
-            const peopleValidatable: Validatable = {
-                value: +enteredPeople,
-                required: true,
-                min: 1,
-                max: 5
-            };
-
-            if (
-                !validate(titleValidatable) ||
-                !validate(descriptionValidatable) ||
-                !validate(peopleValidatable)
-            ) {
-                alert('Invalid input, please try again!');
-                return;
-            } else {
-                return [enteredTitle, enteredDescription, +enteredPeople]; // Returns a tuple. The + sign before the variable converts to int
-            }
-        }
-
-        private clearInputs() {
-            this.titleInputElement.value = '';
-            this.descriptionInputElement.value = '';
-            this.peopleInputElement.value = '';
-        }
+    /**
+     * We are able to get the Class properties values in this method because of the autoBind decorator.
+     * The autoBind decorator binds the Class properties values to this method
+     */
+    @autoBind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
 
         /**
-         * We are able to get the Class properties values in this method because of the autoBind decorator.
-         * The autoBind decorator binds the Class properties values to this method
+         * Since tuple doesn't exist in vanilla javascript we have to check the values like this
          */
-        @autoBind
-        private submitHandler(event: Event) {
-            event.preventDefault();
-            const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
 
-            /**
-             * Since tuple doesn't exist in vanilla javascript we have to check the values like this
-             */
-            if (Array.isArray(userInput)) {
-                const [title, description, people] = userInput;
-
-                state.addProject(title, description, people)
-                this.clearInputs();
-            }
+            state.addProject(title, description, people);
+            this.clearInputs();
         }
     }
 }
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,72 +1,71 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../state/project-state.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { DragTarget } from '../models/drag-drop.js';
+import { Project, ProjectStatus } from '../models/project.js';
+import { Component } from './base-component.js';
+import { autoBind } from '../decorators/autobind.js';
+import { state } from '../state/project-state.js';
+import { ProjectItem } from './project-item.js';
 
-namespace App {
-    export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
-        assignedProjects: Project[];
+export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
+    assignedProjects: Project[];
 
-        constructor(private type: 'active' | 'finished') {
-            super('project-list', 'app', false, `${type}-projects`);
+    constructor(private type: 'active' | 'finished') {
+        super('project-list', 'app', false, `${type}-projects`);
 
-            this.assignedProjects = [];
+        this.assignedProjects = [];
 
-            this.configure();
-            this.renderContent();
-        }
+        this.configure();
+        this.renderContent();
+    }
 
-        @autoBind
-        dragOverHandler(event: DragEvent) {
-            if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
-                event.preventDefault();
-                const listEl = this.element.querySelector('ul')!;
-                listEl.classList.add('droppable');
-            }
-        }
-
-        @autoBind
-        dragLeaveHandler(_: DragEvent): void {
+    @autoBind
+    dragOverHandler(event: DragEvent) {
+        if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+            event.preventDefault();
             const listEl = this.element.querySelector('ul')!;
-            listEl.classList.remove('droppable');
+            listEl.classList.add('droppable');
         }
+    }
 
-        @autoBind
-        dropHandler(event: DragEvent): void {
-            const projectId = event.dataTransfer!.getData('text/plain');
-            state.moveProject(projectId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
-        }
+    @autoBind
+    dragLeaveHandler(_: DragEvent): void {
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.remove('droppable');
+    }
 
-        configure(): void {
-            this.element.addEventListener('dragover', this.dragOverHandler);
-            this.element.addEventListener('dragleave', this.dragLeaveHandler);
-            this.element.addEventListener('drop', this.dropHandler);
+    @autoBind
+    dropHandler(event: DragEvent): void {
+        const projectId = event.dataTransfer!.getData('text/plain');
+        state.moveProject(projectId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished);
+    }
 
-            state.addListener((projects: Project[]) => {
-                this.assignedProjects = projects.filter(project => {
-                    if (this.type === 'active')
-                        return project.status === ProjectStatus.Active;
-                    else
-                        return project.status === ProjectStatus.Finished;
-                });
-                this.renderProjects();
+    configure(): void {
+        this.element.addEventListener('dragover', this.dragOverHandler);
+        this.element.addEventListener('dragleave', this.dragLeaveHandler);
+        this.element.addEventListener('drop', this.dropHandler);
+
+        state.addListener((projects: Project[]) => {
+            this.assignedProjects = projects.filter(project => {
+                if (this.type === 'active')
+                    return project.status === ProjectStatus.Active;
+                else
+                    return project.status === ProjectStatus.Finished;
             });
-        }
+            this.renderProjects();
+        });
+    }
 
-        renderContent() {
-            this.element.querySelector('ul')!.id = `${this.type}-projects-list`;
-            this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
-        }
+    renderContent() {
+        this.element.querySelector('ul')!.id = `${this.type}-projects-list`;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
 
-        private renderProjects() {
-            const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
 
-            listEl.innerHTML = '';
+        listEl.innerHTML = '';
 
-            for (const projectItem of this.assignedProjects) {
-                new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
-            }
+        for (const projectItem of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, projectItem);
         }
     }
 }
